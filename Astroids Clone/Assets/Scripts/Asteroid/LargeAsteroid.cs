c7de50ddf4f6fe507e29cb024f4817e4dc7f6496
cgalvin <conorreecegalvin@gmail.com>
2020-07-30 23:04:34 +0100

    0.0.2: Set up object prefabs, updated object sprites, reorganised asset folders, created asteroids base scripts and interfaces, implemented base functionality for asteroids movement and spawning.

---
+++ Astroids Clone/Assets/Scripts/Asteroid/LargeAsteroid.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeAsteroid : Asteroid
+{
+    public override void TakeDamage()
+    {
+        // TODO split into 2x medium astroids
+
+        base.TakeDamage();
+    }
+}
---
+++ Astroids Clone/Assets/Scripts/Interfaces/IScoreGiver.cs
@@ -0,0 +1,4 @@
+﻿public interface IScoreGiver
+{
+    void GiveScore();
+}
